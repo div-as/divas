e898006cbd9cb964a7d93613c3fdbbbb838a9cea
LauraRodrigues31 <laura.rodrigues@sou.inteli.edu.br>
2024-07-13 07:18:26 -0300

    identação

--- divas_project/api/controllers/UsuarioController.js
+++ divas_project/api/controllers/UsuarioController.js
@@ -1,35 +1,34 @@
 module.exports = {
-    cadastro: async function (req, res) {
-      try {
-        const usuario = await Usuario.create(req.body).fetch();
-        return res.redirect('/login'); // Redireciona para a página de login após o registro
-      } catch (err) {
-        return res.status(400).json({ error: err.message });
+  cadastro: async function (req, res) {
+    try {
+      const usuario = await Usuario.create(req.body).fetch();
+      return res.redirect('/login'); // Redireciona para a página de login após o registro
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  },
+
+  login: async function (req, res) {
+    const { email, senha } = req.body;
+    if (!email || !senha) {
+      return res.status(400).json({ error: 'Email e senha são obrigatórios' });
+    }
+
+    try {
+      const usuario = await Usuario.findOne({ email });
+      if (!usuario) {
+        return res.status(404).json({ error: 'Usuário não encontrado' });
       }
-    },
-  
-    login: async function (req, res) {
-      const { email, senha } = req.body;
-      if (!email || !senha) {
-        return res.status(400).json({ error: 'Email e senha são obrigatórios' });
+
+      const isMatch = await Usuario.comparePassword(senha, usuario.senha);
+      if (!isMatch) {
+        return res.status(401).json({ error: 'Senha inválida' });
       }
-  
-      try {
-        const usuario = await Usuario.findOne({ email });
-        if (!usuario) {
-          return res.status(404).json({ error: 'Usuário não encontrado' });
-        }
-  
-        const isMatch = await Usuario.comparePassword(senha, usuario.senha);
-        if (!isMatch) {
-          return res.status(401).json({ error: 'Senha inválida' });
-        }
-  
-         // Redireciona para a página home passando o nome do usuário como parâmetro
+
+      // Redireciona para a página home passando o nome do usuário como parâmetro
       return res.view('pages/homepage', { nome: usuario.nome });
     } catch (err) {
       return res.status(500).json({ error: err.message });
     }
   }
 }
-  
